d75ed5aedbf6ab7a924aa452fd37e0b8fd702831
Sean Duckett <sduckett@gmail.com>
2015-07-29 19:56:22 -0600

    cleanup stuff that wasn't working for matt

--- init.el
+++ init.el
@@ -1,8 +1,3 @@
-;; load the package management configuration
-(setq package-configuration (expand-file-name "packages.el" user-emacs-directory))
-(when (file-exists-p package-configuration)
-  (load package-configuration))
-
 ;; keep customize settings in their own file
 (setq custom-file "~/.emacs.d/custom.el")
 (when (file-exists-p custom-file)
