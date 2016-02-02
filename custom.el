4a8203971c8ab1df6f7a240956486b5e37a754a1
Sean Duckett <sduckett@gmail.com>
2016-02-01 17:55:37 -0700

    setup helm, tweak fontlock for parens

--- custom.el
+++ custom.el
@@ -3,10 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-agenda-files
+   (quote
+    ("~/docs/org/personal/wedding.org" "~/docs/tscp/tscp.org" "~/docs/org/personal/meetups.org" "~/docs/org/tasks.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mc/cursor-face ((t (:background "color-32" :inverse-video t)))))
+ '(mc/cursor-face ((t (:background "color-32" :inverse-video t))))
+ '(show-paren-match ((t (:background "black" :distant-foreground "black" :foreground "brightred" :weight bold)))))
