409d88cc736e47f96fad68e1fe067973c198bc4f
Sean Duckett <sean.duckett@gateless.com>
2022-01-11 11:52:39 -0700

    cleanup clojure-mode and friends
    - fix save-place-mode
    - drop marmalade (SSL issues, stale)
    - add some modes that I had installed by hand

--- custom.el
+++ custom.el
@@ -4,17 +4,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(haskell-mode-hook
-   (quote
-    (capitalized-words-mode haskell-indent-mode interactive-haskell-mode)))
- '(haskell-process-args-stack-ghci (quote ("--ghci-options=-ferror-spans")))
+   '(capitalized-words-mode haskell-indent-mode interactive-haskell-mode))
+ '(haskell-process-args-stack-ghci '("--ghci-options=-ferror-spans"))
  '(haskell-process-path-ghci "stack ghci")
  '(haskell-tags-on-save t)
  '(org-agenda-files
-   (quote
-    ("~/docs/org/personal/wedding.org" "~/docs/tscp/tscp.org" "~/docs/org/personal/meetups.org" "~/docs/org/tasks.org")))
+   '("~/docs/org/personal/wedding.org" "~/docs/tscp/tscp.org" "~/docs/org/personal/meetups.org" "~/docs/org/tasks.org"))
  '(package-selected-packages
-   (quote
-    (dap-mode lsp-java lsp-ui company-lsp lsp-mode haskell-interactive-mode yaml-mode wc-mode use-package twittering-mode ssh-config-mode smex ruby-test-mode request-deferred rbenv paren-face ob-http markdown-mode+ magit-gh-pulls inf-ruby ido-vertical-mode ido-ubiquitous idle-highlight-mode htmlize hindent highlight-symbol highlight-quoted highlight-numbers helm-swoop helm-projectile helm-ls-git helm-gtags helm-flyspell helm-flycheck helm-flx helm-descbinds helm-ag haskell-mode haskell-emacs gtags gnuplot-mode gnuplot git-timemachine git-gutter git-annex gist fuzzy flycheck-tip expand-region epresent elisp-slime-nav csv-mode company clojure-mode-extra-font-locking clj-refactor bbdb alert ace-jump-mode)))
+   '(window-numbering-mode window-numbering dap-mode lsp-java lsp-ui company-lsp lsp-mode haskell-interactive-mode yaml-mode wc-mode use-package twittering-mode ssh-config-mode smex ruby-test-mode request-deferred rbenv paren-face ob-http markdown-mode+ magit-gh-pulls inf-ruby ido-vertical-mode ido-ubiquitous idle-highlight-mode htmlize hindent highlight-symbol highlight-quoted highlight-numbers helm-swoop helm-projectile helm-ls-git helm-gtags helm-flyspell helm-flycheck helm-flx helm-descbinds helm-ag haskell-mode haskell-emacs gtags gnuplot-mode gnuplot git-timemachine git-gutter git-annex gist fuzzy flycheck-tip expand-region epresent elisp-slime-nav csv-mode company clojure-mode-extra-font-locking clj-refactor bbdb alert ace-jump-mode))
  '(rng-nxml-auto-validate-flag nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
