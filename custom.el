5d35752a0be2b5aa16b2d99c3141840009c4aeb5
Sean Duckett <sduckett@gmail.com>
2023-05-01 10:43:03 -0600

    wip: sync the intel macbook config

--- custom.el
+++ custom.el
@@ -8,16 +8,15 @@
  '(haskell-process-args-stack-ghci '("--ghci-options=-ferror-spans"))
  '(haskell-process-path-ghci "stack ghci")
  '(haskell-tags-on-save t)
- '(org-agenda-files
-   '("~/docs/org/personal/wedding.org" "~/docs/tscp/tscp.org" "~/docs/org/personal/meetups.org" "~/docs/org/tasks.org"))
+ '(js-indent-level 2)
  '(package-selected-packages
-   '(window-numbering-mode window-numbering dap-mode lsp-java lsp-ui company-lsp lsp-mode haskell-interactive-mode yaml-mode wc-mode use-package twittering-mode ssh-config-mode smex ruby-test-mode request-deferred rbenv paren-face ob-http markdown-mode+ magit-gh-pulls inf-ruby ido-vertical-mode ido-ubiquitous idle-highlight-mode htmlize hindent highlight-symbol highlight-quoted highlight-numbers helm-swoop helm-projectile helm-ls-git helm-gtags helm-flyspell helm-flycheck helm-flx helm-descbinds helm-ag haskell-mode haskell-emacs gtags gnuplot-mode gnuplot git-timemachine git-gutter git-annex gist fuzzy flycheck-tip expand-region epresent elisp-slime-nav csv-mode company clojure-mode-extra-font-locking clj-refactor bbdb alert ace-jump-mode))
+   '(lsp-docker treemacs-tab-bar treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs-evil company-quickhelp default-text-scale nav-flash json-rpc-server load-dir magit rainbow-delimiters origami flycheck-clj-kondo flycheck-joker ido-completing-read+ talonscript-mode company-terraform terraform-doc terraform-mode json-mode flymake-json jq-format jq-mode counsel-jq solarized-theme color-theme-sanityinc-solarized zprint-mode svelte-mode ts window-numbering-mode window-numbering dap-mode lsp-java lsp-ui company-lsp lsp-mode haskell-interactive-mode yaml-mode wc-mode use-package twittering-mode ssh-config-mode smex ruby-test-mode request-deferred rbenv paren-face ob-http markdown-mode+ magit-gh-pulls inf-ruby ido-vertical-mode ido-ubiquitous idle-highlight-mode htmlize hindent highlight-symbol highlight-quoted highlight-numbers helm-swoop helm-projectile helm-ls-git helm-gtags helm-flyspell helm-flycheck helm-flx helm-descbinds helm-ag haskell-mode haskell-emacs gtags gnuplot-mode gnuplot git-timemachine git-gutter git-annex gist fuzzy flycheck-tip expand-region epresent elisp-slime-nav csv-mode company clojure-mode-extra-font-locking clj-refactor bbdb alert ace-jump-mode))
  '(rng-nxml-auto-validate-flag nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(linum ((t (:inherit (shadow default) :foreground "blue"))))
+ '(linum ((t (:inherit (shadow default) :foreground "brightgreen"))))
  '(mc/cursor-face ((t (:background "color-32" :inverse-video t))))
  '(show-paren-match ((t (:background "black" :distant-foreground "black" :foreground "brightred" :weight bold)))))
